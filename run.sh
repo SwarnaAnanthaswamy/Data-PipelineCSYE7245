dd9a53aa17c4b711eaa14612678d932da9650d0b
Uthsav Shetty <47194856+UthsavShetty@users.noreply.github.com>
2020-02-08 20:13:48 -0500

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-python 0131_scrapeSEC.py \
-	--project encoded-site-265218 \
-	--runner DataflowRunner \
-	--staging_location gs://demo-temp-bucket/staging \
-	--temp_location gs://demo-temp-bucket/temp \
-	--output gs://demo-temp-bucket/output2
